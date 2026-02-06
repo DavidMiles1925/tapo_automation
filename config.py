27f433afeb64842843a1f5894c6fc0350ad66049
David Miles <davidmiles1925@gmail.com>
2026-02-05 20:23:43 -0600

    Stop tracking config.py and ignore it

--- config.py
+++
@@ -1,4 +0,0 @@
-TAPO_USERNAME = "tapo_username"
-TAPO_PASSWORD = "tapo_passowrd"
-PLUG_IP_1 = "192.168.0.230" # A1 Mini, which is a model 115
-PLUG_IP_2 = "192.168.0.231" # A1 Mini, which is a model 115
